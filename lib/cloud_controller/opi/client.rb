e06870d429f4372366384a0355f1de2b14d7498a
Mario Nitchev <marionitchev@gmail.com>
2018-06-06 18:55:48 +0300

    Replace BbsAppsClient with OPI::Client
    Change OPI::Client method signatures and add stubbed methods.
    
    [#158067656]
    
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>
    
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

--- lib/cloud_controller/opi/client.rb
+++ lib/cloud_controller/opi/client.rb
@@ -1,5 +1,6 @@
 require 'uri'
 require 'httpclient'
+require 'multi_json'
 
 module OPI
   class Client
@@ -7,25 +8,46 @@ module OPI
       @opi_url = URI(opi_url)
     end
 
-    def desire_lrp(lrp)
+    def desire_app(process)
       client = HTTPClient.new
-      @opi_url.path = "/apps/#{lrp.process_guid}"
-      client.put(@opi_url,
-        body: {
-          imageUrl: lrp.image_url,
-          command: lrp.command,
-          env: lrp.env,
-          targetInstances: lrp.target_instances
-        }.to_json
-        # TODO: Note that in the spike we had to use MultiJSON in order to get around a strange encoding issue
-      )
+      @opi_url.path = "/apps/#{process.process_guid}"
+      body = {
+            process_guid: process.process_guid,
+            docker_image: process.current_droplet.docker_receipt_image,
+            start_command: process.command,
+            env: convert(process.environment_json),
+            num_instances: process.desired_instances,
+            droplet_hash: process.current_droplet.droplet_hash
+        }
+      client.put(@opi_url, body: MultiJson.dump(body))
     end
 
-    def desired_lrps
+    def convert(hash)
+      hash.map do |k, v|
+        { 'name' => k.to_s, 'value' => v.to_s }
+      end
+    end
+
+    def fetch_scheduling_infos
       client = HTTPClient.new
       @opi_url.path = '/apps'
 
       client.get(@opi_url)
+    end
+
+    # We need these methods to exists in order to run our tests.
+    def update_app(process_guid, lrp_updat); end
+
+    def get_app(process_guid)
+      ::Diego::Bbs::Models::DesiredLRP.new(
+        process_guid: process_guid,
+        routes: [],
+      )
+    end
+
+    def stop_app(process_guid); end
+
+    def bump_freshness
     end
   end
 end
--- spec/opi/client_spec.rb
+++ spec/opi/client_spec.rb
@@ -3,7 +3,7 @@ require 'webmock/rspec'
 
 module OPI
   RSpec.describe Client do
-    describe '#desire_lrp' do
+    describe '#desire_app' do
       let(:opi_url) { 'http://opi.service.cf.internal:8077' }
       subject(:client) {
         Client.new(opi_url)
@@ -12,13 +12,11 @@ module OPI
       let(:lrp) {
         double(
           process_guid: 'guid_1234',
-          image_url: img_url,
-          command: ['ls', '-la'],
-          env: {
-            'PORT' => '8080',
-            'FOO' => 'BAR'
-          },
-          target_instances: 4
+          version: '0.1.0',
+          current_droplet: double(docker_receipt_image: img_url, droplet_hash: 'd_haash'),
+          command: 'ls -la',
+          environment_json: { 'PORT': 8080, 'FOO': 'BAR' },
+          desired_instances: 4
        )
       }
 
@@ -28,27 +26,27 @@ module OPI
             to_return(status: 201)
         end
         it 'sends a PUT request' do
-          response = client.desire_lrp(lrp)
+          response = client.desire_app(lrp)
 
           expect(response.status_code).to equal(201)
           expect(WebMock).to have_requested(:put, "#{opi_url}/apps/guid_1234").
-            with(body: {
-              imageUrl: img_url,
-              command: ['ls', '-la'],
-              env: {
-                'PORT' => '8080',
-                'FOO' => 'BAR'
-              },
-              targetInstances: 4
-            }.to_json
-          )
+            with(body: MultiJson.dump({
+              process_guid: 'guid_1234',
+              docker_image: img_url,
+              start_command: 'ls -la',
+              env: [{ name: 'PORT', value: '8080' }, { name: 'FOO', value: 'BAR' }],
+              num_instances: 4,
+              droplet_hash: 'd_haash'
+            }
+          ))
         end
       end
     end
-    describe '#get_lrps' do
+    describe '#fetch_scheduling_infos' do
       let(:opi_url) { 'http://opi.service.cf.internal:8077' }
       let(:lrp) { double(lrps: [
         double(
+          process_guid: 'guid_1234',
           imageUrl: 'http://example.org/image1234',
           command: ['ls', ' -la'],
           env: {
@@ -58,6 +56,7 @@ module OPI
           targetInstances: 4
         ),
         double(
+          process_guid: 'guid_5678',
           imageUrl: 'http://example.org/image5678',
           command: ['rm', '-rf', '/'],
           env: {
@@ -69,6 +68,7 @@ module OPI
       }
       let(:expected_body) { { lrps: [
         {
+          process_guid: 'guid_1234',
           imageUrl: 'http://example.org/image1234',
           command: ['ls', ' -la'],
           env: {
@@ -78,6 +78,8 @@ module OPI
           targetInstances: 4
         },
         {
+          process_guid: 'guid_5678',
+
           imageUrl: 'http://example.org/image5678',
           command: ['rm', '-rf', '/'],
           env: {
@@ -99,7 +101,7 @@ module OPI
         end
 
         it 'propagates the response' do
-          response = client.desired_lrps
+          response = client.fetch_scheduling_infos
           expect(WebMock).to have_requested(:get, "#{opi_url}/apps")
           expect(response.body).to eq(expected_body)
 
