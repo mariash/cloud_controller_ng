34748b1c8d0854ee4ccb701a547fdc426ef23dcf
Mario Nitchev <marionitchev@gmail.com>
2018-06-27 12:37:37 +0300

    Implement OPI::InstancesClient
        * Modify dependency_locator to use OPI::InstancesClient if OPI is enabled.
        * Update eirini apps endpoints
        * Add OPI integration tests
    
    [#157657129]
    
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>
    Signed-off-by: Mario Nitchev <marionitchev@gmail.com>

---
+++ lib/cloud_controller/opi/instances_client.rb
@@ -0,0 +1,61 @@
+require 'httpclient'
+require 'json'
+require 'cloud_controller/errors/instances_unavailable'
+require 'cloud_controller/errors/no_running_instances'
+
+module OPI
+  class InstancesClient
+    ActualLRPKey = Struct.new(:index, :process_guid)
+
+    class ActualLRP
+      attr_reader :actual_lrp_key
+      attr_reader :state
+      attr_reader :since
+      attr_reader :placement_error
+
+      def initialize(actual_lrp_key, state)
+        @actual_lrp_key = actual_lrp_key
+        @state = state
+        @since = 0
+        @placement_error = ''
+      end
+
+      def ==(other)
+        other.class == self.class && other.actual_lrp_key == @actual_lrp_key
+      end
+    end
+
+    def initialize(opi_url)
+      @opi_url = URI(opi_url)
+    end
+
+    def lrp_instances(process)
+      client = HTTPClient.new
+      @opi_url.path = "/apps/#{process.guid}/instances"
+      begin
+        retries ||= 0
+        resp = client.get(@opi_url)
+        resp_json = JSON.parse(resp.body)
+        handle_error(resp_json)
+      rescue => e
+        retry if (retries += 1) < 5
+        raise e
+      end
+      process_guid = resp_json['process_guid']
+      resp_json['instances'].map do |instance|
+        ActualLRP.new(ActualLRPKey.new(instance['index'], process_guid), instance['state'])
+      end
+    end
+
+    def desired_lrp_instance(process); end
+
+    private
+
+    def handle_error(response_body)
+      error = response_body['error']
+      return unless error
+
+      raise CloudController::Errors::NoRunningInstances.new('No running instances')
+    end
+  end
+end
